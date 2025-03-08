73a2ae7ff281e12f4153c8f1e25fa0891ac18cfa
Yordy Carmona <yordy.carmona8@gmail.com>
2025-03-08 12:50:16 -0300

    Feat: add global variables to use in scripts

---
+++ os/window/config/global-vars.ps1
@@ -0,0 +1,92 @@
+# Directorios
+$configPath = "$env:USERPROFILE\.config"
+$workspacePath = "$env:USERPROFILE\workspace"
+$reposPath = "$env:workspacePath\repos"
+$dotfilesPath = "$env:reposPath\dotfiles-2024"
+$gitConfigDotfilesPath = "$env:dotfilesPath\git\.gitconfig"
+$starshipDotfilesPath = "$env:dotfilesPath\os\cross-platform\starship\starship.toml"
+$wallpapersPath = "$env:reposPath\wallpapers"
+$obsidianPath = "$env:reposPath\obsidian-notes"
+$nvimPath = "$env:LOCALAPPDATA\nvim"
+
+# Archivos u otros
+$starshipPath = "$env:configPath\starship.toml"
+$scoopExe = "$env:USERPROFILE\scoop\scoop.exe"
+$gitConfig = "$env:USERPROFILE\.gitconfig"
+$ahkFilePath = "$env:USERPROFILE\repos\dotfiles\os\windows\remap_keys\remap.ahk"
+
+# Setup PowerShell profile
+$profilePSDotfilesPath = "$env:dotfilesPath\os\window\programs\PowerShell\profiles\user_profile.ps1"
+$powershellConfigPath= "$configPath\powershell"
+$userProfileConfigPath = "$powershellConfigPath\user_profile.ps1"
+$ProfilePSContent = ". $configPath\powershell\user_profile.ps1"
+
+# Aplicaciones Scoop
+$scoopApps = @(
+  "7zip",
+  "autohotkey",
+  "bitwarden",
+  "discord",
+  "everything",
+  "flow-launcher",
+  "hoppscotch",
+  "obsidian",
+  "powershell",
+  "postman",
+  "vlc",
+  "whatsapp",
+  "windows-terminal",
+  # CLI
+  "bat",
+  "cmake",
+  "curl",
+  "delta",
+  "dotnet-sdk",
+  "docker",
+  "docker-compose",
+  "fnm",
+  "fzf",
+  "gcc",
+  "gh",
+  "httpie",
+  "jq",
+  "lazygit",
+  "lazydocker",
+  "neovim",
+  "oh-my-posh",
+  "pnpm",
+  "posh-git",
+  "posh-docker",
+  "psfzf",
+  "psreadline",
+  "ripgrep",
+  "starship",
+  "sudo",
+  "terminal-icons",
+  "z",
+  # DataBases
+  "postgresql",
+  "mongodb",
+  "tableplus",
+  # IDEs
+  "vscode",
+  "rider",
+  "pycharm",
+  "clion",
+  "webstorm",
+  "datagrip",
+  # browsers
+  "firefox",
+  "googlechrome",
+  "brave",
+  "edge"
+  # nerdfonts
+  "CascadiaCode-NF-Mono"
+  "CascadiaMono-NF-Mono"
+  "FiraCode-NF-Mono"
+  "FiraMono-NF-Mono"
+  "Hack-NF-Mono"
+  "JetBrainsMono-NF-Mono"
+  "Meslo-NF-Mono"
+  "ZedMono-NF-Mono"
+)
