df55c1f3d2cae8b17082310e3a9b2209e8952471
Yordy Carmona <yordy.carmona8@gmail.com>
2025-04-27 19:20:43 -0400

    feat: update symlinks

--- setup-symlinks.sh
+++ setup-symlinks.sh
@@ -8,6 +8,10 @@ ln -s $DOTFILES/os/cross-platform/starship/ ~/.config/
 ln -fs $DOTFILES/git/.gitconfig ~/.gitconfig
 ln -fs $DOTFILES/shell/zsh/.zshrc ~/.zshrc
 
+## cpp
+ln -sf $DOTFILES/os/cross-platform/clangd/.clang-format ~/.clang-format
+ln -sf $DOTFILES/os/cross-platform/clangd/.clangd ~/.clangd
+
 # If Windows...
 
 # If macOs...
