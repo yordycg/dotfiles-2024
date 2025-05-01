e7de10d870ac2d02cfbd530be274a3bd17fdb65e
Yordy Carmona <yordy.carmona8@gmail.com>
2025-05-01 11:12:30 -0400

    feat (git): add new git config, new aliases and a template for commits

--- shell/aliases.sh
+++ shell/aliases.sh
@@ -25,43 +25,79 @@ alias ~="cd ~"
 
 # Git
 alias g="git"
+alias gi="git init"
 alias gcl="git clone"
+##########
 alias ga="git add"
 alias gaa="git add -A"
-alias gacm="git add . && git commit -m" # Agregar y hacer commit, cualquier archivo
-alias gc="git commit"
+##########
+alias gc="git commit" # open editor
 alias gcm="git commit -m"
-alias gca="git commit -am" # Agregar y hacer commit, no toma en cuenta archivos nuevos
-alias gcam="git commit --amend -m" # Editar el MENSAJE del ultimo commit
-alias gcane="git add --all && git commit --amend --no-edit" # Util para agregar cambios olvidados al ultimo commit
-alias gco="git checkout"
-alias gcob="git checkout -b"
+alias gca="git commit -a"   # add modified files and open editor
+alias gcam="git commit -am" # add modified files with "message"
+##########
 alias gcp="git cherry-pick"
+##########
+alias gst="git stash"
+alias gsts="git stash save"
+alias gstl="git stash list"
+alias gstp="git stash pop" # apply last stash
+alias gstd="git stash drop"
+alias gstc="git stash clear"
+##########
+alias gt="git tag"
+alias gtd="git tag -d" # delete tag
+##########
 alias gd="git diff"
 alias gds="git diff --staged"
-alias gf="git fetch --all -p"
+alias gdc="git diff --cached" # show diff in staged vs HEAD
+##########
 alias gs="git status"
 alias gss="git status -sb"
+##########
 alias grb="git rebase"
 alias grb3="git rebase -i HEAD~3"
 alias grb5="git rebase -i HEAD~5"
+alias grba="git rebase --abort"
+alias grbc="git rebase --continue"
+alias grbi="git rebase --interactive"
+##########
 alias gps="git push"
-alias gpsf="git push --force"
+alias gpsf="git push --force-with-lease"
 alias gpl="git pull --rebase --autostash"
+alias gf="git fetch --all -p"
 alias gpp="git pull && git push"
+##########
 alias gm="git merge"
+alias gma="git merge --abort"
+alias gmc="git merge --continue"
+##########
+alias gco="git checkout"     # change a branch
+alias gcob="git checkout -b" # new branch and change
 alias gb="git branch"
-alias gbr="git branch --format='%(HEAD) %(color:yellow)%(refname:short)%(color:reset) - %(contents:subject) %(color:green)(%(committerdate:relative)) - <%(authorname)>' --sort=-committerdate"
-alias gl="git --no-pager log --graph --pretty=oneline --abbrev-commit --decorate --all"
-alias glg="git log --graph --all --abbrev-commit --date=relative --pretty=format:'%C(yellow)%h%C(reset) -%C(red)%d%C(reset) %s %C(green)(%ar) %C(bold blue)<%an>%C(reset)'"
+alias gbr="git branch --format='%(HEAD) %(color:yellow)%(refname:short)%(color:reset) - %(contents:subject) %(color:green)(%(committerdate:relative)) - <%(authorname)>'"
+##########
+alias gl="git log --graph --all --pretty=oneline --decorate"
+alias glg="git log --graph --all --date=relative --pretty=format:'%C(magenta)%h %C(white) %an  %ar%C(auto)  %D%n%s%n'"
+# alias glg="git log --graph --all --abbrev-commit --date=relative --pretty=format:'%C(yellow)%h%C(reset) -%C(red)%d%C(reset) %s %C(green)(%ar) %C(bold blue)<%an>%C(reset)'"
 # alias gg="git log --graph --abbrev-commit --date=relative --pretty=format:'%C(bold)%h%C(reset)%C(magenta)%d%C(reset) %s %C(yellow)<%an> %C(cyan)(%cr)%C(reset)'"
 
-# Git witth fzf
-alias gafzf='git ls-file -m -o --exclude-standar | grep -v "__pycache__" | fzf -m --print0 | xargs -0 -o -t git add' # Git add con fzf
-alias grmfzf='git ls-file -m -o --exclude-standar | fzf -m --print0 | xargs -0 -o -t git rm' # Git rm con fzf
-alias grfzf='git diff --name-only | fzf -m --print0 | xargs -0 -o -t git restore' # Git restore con fzf
-alias grsfzf='git diff --name-only | fzf -m --print0 | xargs -0 -o -t git restore --staged' # Git restore --staged con fzf
-alias gbfzf='git branch | fzf | xargs git checkout' # Seleccionar una branch con fzf
+# %h - commit hash
+# %an - author name
+# %ar - commit time
+# %D - ref names
+# %n - new line
+# %s - commit message
+
+# Git add with fzf
+alias gafzf='git ls-file -m -o --exclude-standar | grep -v "__pycache__" | fzf -m --print0 | xargs -0 -o -t git add'
+# Git rm wthh fzf
+alias grmfzf='git ls-file -m -o --exclude-standar | fzf -m --print0 | xargs -0 -o -t git rm'
+# Git restore with fzf
+alias grfzf='git diff --name-only | fzf -m --print0 | xargs -0 -o -t git restore'
+alias grsfzf='git diff --cached --name-only | fzf -m --print0 | xargs -0 -o -t git restore --staged'
+# Selected a branch with fzf
+alias gbfzf='git branch | fzf | xargs git checkout'
 
 alias lg="lazygit" # need install lazygit
 
@@ -101,20 +137,19 @@ alias dcr="dcd && dcu -d"
 alias ld="lazydocker" # need install lazydocker
 
 # Utils
-alias setup="mkdir \"$1\" && cd \"$1\""
 alias eal="$EDITOR $DOTFILES/shell/aliases.sh"
 alias ezsh="$EDITOR ~/.zshrc"
 alias ebash="$EDITOR ~/.bashrc"
 alias eatty="$EDITOR ~/.config/alacritty/alacritty.toml"
-alias ekitty="$EDITOR $DOTFILES/os/linux/kitty/kitty.conf"
+alias ektty="$EDITOR $DOTFILES/os/linux/kitty/kitty.conf"
 alias ewterm="$EDITOR $DOTFILES/os/cross-platform/wezterm/wezterm.lua"
 alias sbash="source ~/.bashrc"
 alias szsh="source ~/.zshrc"
 alias t="tmux"
 alias tks="tmux kill-server"
-alias cat="bat" # instalar desde 'bat/github'
+alias cat="bat" # install 'bat/github'
 alias ff="fastfetch"
-# alias wifi="nmtui"
+alias wifi="nmtui"
 
 # Search
 alias grep="grep --color=auto"
